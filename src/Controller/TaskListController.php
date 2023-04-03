5d23f4026a2facd8d0552f7d4899b2290f44282d
Liamcr <cariou.liam@orange.fr>
2023-04-03 12:27:21 +0200

    debug et style task

--- src/Controller/TaskListController.php
+++ src/Controller/TaskListController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
-#[Route('/task/list')]
+#[Route('/tasklist')]
 class TaskListController extends AbstractController
 {
     #[Route('/', name: 'app_task_list_index', methods: ['GET'])]
